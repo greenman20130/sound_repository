01e505e59ee9a87b3820c28ff8af7f5945994ea0
Алексей Шелестов <63450993+greenman20130@users.noreply.github.com>
2025-03-31 11:17:42 +0300

    created db
    created db engine

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ from sqlalchemy import select
 from datetime import timedelta
 from starlette.responses import RedirectResponse
 
+from database import get_database_session, engine
 app = FastAPI(
     title="Audio Repository Platform",
     openapi_url="/openapi.json",
     docs_url="/docs",
     redoc_url="/redoc",
 )
+
+os.makedirs(settings.AUDIO_FILES_DIR, exist_ok=True)
+
+@app.on_event("startup")
+async def initialize_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@app.get("/")
+async def zeropoint():
+    return {"message": "Hello, how are you?"}
